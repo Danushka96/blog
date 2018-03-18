7095c648fd72ae321403899f2a1ac5010b22e483
danushka96 <danushkaherath96@gmail.com>
2018-03-18 11:10:50 +0530

    add Hash method

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use App\User;
 use App\role;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Input;
 
 class UserController extends Controller
 {
@@ -18,9 +20,24 @@ class UserController extends Controller
         return view('Admin/users/new');
     }
     public function store(Request $request){
-        $user=User::create($request->all());
+//        $request->user()->fill([
+//            'password' => Hash::make($request->password)
+//        ])->save();
+        $usernew = new User();
+        $usernew -> name= $request->name;
+        $usernew -> password= Hash::make($request->input('password'));
+        $usernew -> email=$request->email;
+        $usernew -> role=$request->role;
+        $usernew -> fname=$request->fname;
+        $usernew -> lname=$request->lname;
+        $usernew -> save();
+//        $user=User::create($request->all());
         $user=User::all();
         $role=role::all();
         return view('Admin/users/index',compact('user','role'));
+    }
+    public function view($userid){
+        $current=User::findorFail($userid);
+        return view('Admin/users/view',compact($current));
     }
 }
